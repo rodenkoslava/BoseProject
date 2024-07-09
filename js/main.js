0982fa92149dd0c543129c017b15ecc726244564
rodenkoslava <131722989+rodenkoslava@users.noreply.github.com>
2024-07-09 12:47:41 +0200

    bureger-menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerMenu = document.getElementById("hamburger");
+  const navLinks = document.getElementById("menu__list");
+  const closeIcon = document.getElementById("closeMenu");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  burgerMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+  closeIcon.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+
+  // Закрытие меню при клике вне его области
+  document.addEventListener("click", (event) => {
+    if (
+      !burgerMenu.contains(event.target) &&
+      !navLinks.contains(event.target)
+    ) {
+      navLinks.classList.remove("active");
+    }
+  });
+
+  // Закрытие меню при нажатии на кнопку ESC
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      navLinks.classList.remove("active");
+    }
+  });
+});
